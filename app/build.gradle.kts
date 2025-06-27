093f13f9e760240dbc1c844b185922cc86d3cbc3
okami-horo <202130310192@stu.shmtu.edu.cn>
2025-06-27 11:59:48 +0800

    更新release action触发

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -94,7 +94,7 @@ android {
             applicationIdSuffix = ".debug"
         }
         create("r8Test") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -103,7 +103,7 @@ android {
             signingConfig = signingConfigs.findByName("key")
         }
         create("alpha") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
